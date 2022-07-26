a57d33ebfca0e56022d4fd6886943dbf8d37af18
devillu <devillu843@163.com>
2022-07-26 15:34:02 +0800

    增加了多分支的对比试验，双分支大小出了bug

--- Backbone/ResNet.py
+++ Backbone/ResNet.py
@@ -324,6 +324,20 @@ def resnet101_three_alone(num_classes=1000, include_top=True):
 def resnet152_three_alone(num_classes=1000, include_top=True):
     return ResNet_three_part_alone(BasicBlock, [3, 8, 36, 3], num_classes=num_classes, include_top=include_top)
 
+# def getModelSize(model):
+#     param_size = 0
+#     param_sum = 0
+#     for param in model.parameters():
+#         param_size += param.nelement() * param.element_size()
+#         param_sum += param.nelement()
+#     buffer_size = 0
+#     buffer_sum = 0
+#     for buffer in model.buffers():
+#         buffer_size += buffer.nelement() * buffer.element_size()
+#         buffer_sum += buffer.nelement()
+#     all_size = (param_size + buffer_size) / 1024 / 1024
+#     print('模型总大小为：{:.3f}MB'.format(all_size))
+#     return (param_size, param_sum, buffer_size, buffer_sum, all_size)
 
 if __name__ == '__main__':
 
